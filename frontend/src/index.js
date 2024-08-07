93a981db1d5baeb8cf30cebc4174b072f24c8cdb
q10357 <isabel@chaparian.no>
2024-08-07 02:10:41 +0200

    migrated from npm to yarn

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./theme.scss";
 import "./index.css"; // Import global styles from styles folder
 import { App } from "components"; // Import App component
+import { reportWebVitals } from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+
+reportWebVitals();
